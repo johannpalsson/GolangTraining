4c280741a88e3107c8e089dd2207c6850723a8fc
johannpalsson <johann@qpons.is>
2017-04-15 13:14:29 +0000

    adds http.Get code

---
+++ 03_LanguageFundamentals/05_BlankIdentifier/02_erfostudagur/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http" // import the package with "get" function
+)
+
+func main() {
+	res, err := http.Get("http://www.erfostudagur.is/") //Error checking // Get returns a non-nil error number if it fails to open the page
+	if err != nil {                                     //Error Checking
+		log.Fatal(err) //Error Checking
+	}
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil { // If "err" is not equal to "nil"
+		log.Fatal(err) // display error message for "err"
+	}
+	fmt.Printf("%s", page) // Print variable "page" as a string
+}
